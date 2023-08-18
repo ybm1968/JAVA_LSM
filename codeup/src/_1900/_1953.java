e8965512a1577c334da335c36ff9085049e02534
ybm1968 <yby1969@naver.com>
2023-08-18 09:40:13 +0900

    516

--- codeup/src/_1900/_1953.java
+++ codeup/src/_1900/_1953.java
@@ -1,13 +1,20 @@
 package _1900;
 
+import java.util.Scanner;
+
 public class _1953 {
 	
 	public static void method(int n) {
-		
+		//method(n);
+		if( n == 0 ) return;
+		method(n-1);
+		System.out.print("*");
 	}
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		
+		method(n);
 	}
-
 }
