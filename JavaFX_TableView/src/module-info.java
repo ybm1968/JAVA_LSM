b91dc89bc23e8c66f6d0e9f4dab09ea9016ce065
ybm1968 <yby1969@naver.com>
2023-08-23 02:17:43 +0900

    adad

--- JavaFX_TableView/src/module-info.java
+++ JavaFX_TableView/src/module-info.java
@@ -2,7 +2,7 @@ module JavaFX_Stage {
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+	requires java.sql;
 	
 	opens application to javafx.base, javafx.graphics, javafx.fxml;
 }
